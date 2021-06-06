cbefce47fe377cfd8fd69ab13d1fc5eb444abab5
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2021-06-06 15:09:05 +0900

    Study course programmers 순위검색
    Study course programmers 순위검색

---
+++ Progrmmers/src/all/SearchScore.java
@@ -0,0 +1,70 @@
+package all;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+
+
+public class SearchScore {
+
+	public static int[] solution(String[] info, String[] query) {
+		int[] answer = new int[query.length];
+
+		String[] Language = new String[info.length];
+		String[] part = new String[info.length];
+		String[] career = new String[info.length];
+		String[] food = new String[info.length];
+		String[] score = new String[info.length];
+
+		for(int i = 0; i < info.length; i++) {
+			
+			String[] str = info[i].split(" ");
+			Language[i] = str[0];
+			part[i] = str[1];
+			career[i] = str[2];
+			food[i] = str[3];
+			score[i] = str[4];
+		}
+		
+		for(int i = 0; i < query.length; i++) {
+			query[i].replaceAll(" and ", " ");
+			String[] query_str = query[i].split(" ");
+			
+			
+			
+		}
+		
+		
+		
+
+
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		String[] info = {"java backend junior pizza 150","python frontend senior chicken 210","python frontend senior chicken 150","cpp backend senior pizza 260","java backend junior chicken 80","python backend senior chicken 50"};
+		String[] query = {"java and backend and junior and pizza 100","python and frontend and senior and chicken 200","cpp and - and senior and pizza 250","- and backend and senior and - 150","- and - and - and chicken 100","- and - and - and - 150"};
+
+		int [] result = solution(info, query);
+		for(int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+	}
+}
+
+
+
+
+
+
+//mid : for(int j = 0; j < query.length; j++) {
+//	String[] query_str = query[j].split(" ");
+//	int num = 0;
+//	for(int count = 0; count < info_str.length; count++) {
+//		if(info_str[count].equals(query_str[num]) || query_str[num].equals("-") ||(num == 7 && Integer.parseInt(info_str[count]) > Integer.parseInt(query_str[num]))) {
+//			num = num != 6 ? num+2 : num+1;
+//		}else continue mid;
+//
+//	}
+//	answer[j] += 1;
