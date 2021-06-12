cf2bb7433be2a08774fe55144fe7f408abff63f0
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2021-06-12 13:57:25 +0900

    Study programmers 행렬의 곱셈
    Study programmers 행렬의 곱셈

---
+++ Progrmmers/src/all/LeastMultiple.java
@@ -0,0 +1,41 @@
+package all;
+
+import java.util.Arrays;
+
+public class LeastMultiple {
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+    	int raw = arr1.length;
+    	int column = arr2[0].length;
+        int[][] answer = new int[raw][column];
+        
+        for(int i = 0; i < raw; i++) {
+        	for(int j = 0; j < column; j++) {
+        		answer[i][j] = racol(arr1, arr2, i, j);
+        	}
+        }
+        return answer;
+    }
+	
+    public static int racol(int[][] arr1, int[][] arr2, int i, int j) {
+    	int result = 0;
+    	
+    	int r = 0;
+    	while(r < arr1[i].length) {
+    		result += arr1[i][r] * arr2[r++][j]; 
+    	}
+    	
+    	return result;
+    }
+	public static void main(String[] args) {
+		int[][] arr1 = {{2,3,2},{4,2,4},{3,1,4}};
+		int[][] arr2 = {{5,4,3},{2,4,1},{3,1,1}};
+		
+		int[][] result = solution(arr1, arr2);
+		for(int i = 0; i < result.length; i++) {
+			for(int j = 0; j < result[i].length; j++) {
+				System.out.print(result[i][j] + " ");
+			}
+			System.out.println(" ");
+		}
+	}
+}
