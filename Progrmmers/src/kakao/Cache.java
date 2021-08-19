933217bf83d1a6298d2b4e828ffb8bab49858f60
yujin0131 <65699860+yujin0131@users.noreply.github.com>
2021-08-19 16:30:50 +0900

    study programmers 캐시
    study programmers 캐시

--- Progrmmers/src/kakao/Cache.java
+++ Progrmmers/src/kakao/Cache.java
@@ -1,21 +1,37 @@
 package kakao;
 
-import sun.misc.LRUCache;
+import java.util.LinkedList;
 
 public class Cache {
 	public static int solution(int cacheSize, String[] cities) {
+		
 		int answer = 0;
 		int size = cities.length;
-		LRUCache<N, V>
+		LinkedList<String> cache = new LinkedList<>();
+		String city = "";
+		
 		for(int i = 0; i < size; i++) {
+			city = cities[i].toUpperCase();
 			
+			if(cache.contains(city)) {
+				cache.remove(city);
+				answer++;
+			}else {
+				answer += 5;
+			}
+			
+			cache.add(city);
+			
+			if(cache.size() > cacheSize) {
+				cache.removeFirst();
+			}
 		}
 		return answer;
 	}
 	
 	public static void main(String[] args) {
-		int cacheSize = 3;
-		String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "Jeju", "Pangyo", "Seoul", "NewYork", "LA"};
+		int cacheSize = 5;
+		String[] cities = {"Jeju", "Pangyo", "Seoul", "NewYork", "LA", "SanFrancisco", "Seoul", "Rome", "Paris", "Jeju", "NewYork", "Rome"};
 		
 		int result = solution(cacheSize, cities);
 		System.out.println(result);
